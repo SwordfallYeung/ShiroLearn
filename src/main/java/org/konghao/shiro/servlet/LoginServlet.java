e133c3e430ead78b3d2694d56d46d5909aa618e8
yang <2506597416@qq.com>
2016-06-07 17:06:44 +0800

    first commit

---
+++ src/main/java/org/konghao/shiro/servlet/LoginServlet.java
@@ -0,0 +1,58 @@
+package org.konghao.shiro.servlet;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 25065 on 2016/6/6.
+ */
+public class LoginServlet extends HttpServlet {
+    public LoginServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String username=req.getParameter("username");
+        String password=req.getParameter("password");
+
+        Subject subject= SecurityUtils.getSubject();
+//        用户名，密码验证/身份凭证，相当于身份证，这时并没有与给定的用户/密码进行验证
+        UsernamePasswordToken token=new UsernamePasswordToken(username,password);
+       //可能是保存到session里面
+
+        String emsg=null;
+        try {
+//            这里进行真正的验证
+            subject.login(token);
+        } catch (UnknownAccountException e) {
+            emsg="用户名出错!";
+        }catch (IncorrectCredentialsException e) {
+            emsg="用户密码出错!";
+        }catch (AuthenticationException e) {
+            emsg="其他异常："+e.getMessage();
+        }
+
+        if (emsg!=null){
+            req.setAttribute("emsg",emsg);
+            req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req,resp);
+        }else {
+            resp.sendRedirect(req.getContextPath()+"/");
+        }
+    }
+}
---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/webapp/admin/index.jsp
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Insert title here</title>
+</head>
+<body>
+<h1>/admin/index</h1>
+</body>
+</html>
